b6deef72299f5218b633ec9ddfed4bb632d23792
Tristan Marsell <tristan.marsell@t-online.de>
2017-03-26 14:23:00 +0200

    sound: Introduce PDesireAudio 10.1
    Changelog:
    	- Switch for Static mode
    	- Remove Compander Masks
    	- Updated API
    	- Misc Patches
    
    The commit c5be7cba66cdeb3f75f491295c2ecbdaad7f66ba is required to get PDesireAudio 10.1 work

---
+++ include/linux/pdesireaudio/api.h
@@ -0,0 +1,33 @@
+/* Copyright (C) 2017 Tristan Marsell (tristan.marsell@t-online.de). All rights reserved.
+ * Copyright (C) 2017 Team DevElite. All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+ 
+#include "../../../sound/soc/codecs/pdesireaudio.h"
+
+extern void enable_pdesireaudio(void) {
+	pdesireaudio_start();
+	pdesireaudio_init();
+}
+
+extern void disable_pdesireaudio(void) {
+	pdesireaudio_remove();
+	pdesireaudio_init();
+}
+
+extern void reinit_pdesireaudio(void) {
+	pdesireaudio_init();
+}
+
+extern void static_mode_pdesireaudio(bool enable) {
+	pdesireaudio_api_static_mode_control(enable);
+}
+
