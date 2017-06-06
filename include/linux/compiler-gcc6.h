f9ad92075929d7ccfcf48944a036176093ee4553
Tristan Marsell <tristan.marsell@t-online.de>
2017-06-06 18:10:39 +0200

    Make Changes to prepare Satsuki kernel

--- include/linux/compiler-gcc6.h
+++ include/linux/compiler-gcc6.h
@@ -1,5 +1,5 @@
 #ifndef __LINUX_COMPILER_H
-#error "Please don't include <linux/compiler-gcc5.h> directly, include <linux/compiler.h> instead."
+#error "Please don't include <linux/compiler-gcc6.h> directly, include <linux/compiler.h> instead."
 #endif
 
 #define __used				__attribute__((__used__))
@@ -53,7 +53,6 @@
  *   http://gcc.gnu.org/bugzilla/show_bug.cgi?id=58670
  *
  * Work it around via a compiler barrier quirk suggested by Jakub Jelinek.
- * Fixed in GCC 4.8.2 and later versions.
  *
  * (asm goto is automatically volatile - the naming reflects this.)
  */
