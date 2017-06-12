3ed7cf060cc201dde2beb97f3b5ba77696f15f54
Tristan Marsell <tristan.marsell@t-online.de>
2017-06-12 11:22:49 +0200

    BACKPORT: cpufreq: Architecture specific callback for frequency changes
    Architectures that don't have any other means for tracking cpu frequency
    changes need a callback from cpufreq to implement a scaling factor to
    enable scale-invariant per-entity load-tracking in the scheduler.
    
    To compute the scale invariance correction factor the architecture would
    need to know both the max frequency and the current frequency. This
    patch defines weak functions for setting both from cpufreq.
    
    Related architecture specific functions use weak function definitions.
    The same approach is followed here.
    
    These callbacks can be used to implement frequency scaling of cpu
    capacity later.
    
    Cc: Rafael J. Wysocki <rjw@rjwysocki.net>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    Signed-off-by: Tristan Marsell <tristan.marsell@t-online.de>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -275,6 +275,10 @@ static inline void adjust_jiffies(unsigned long val, struct cpufreq_freqs *ci)
 }
 #endif
 
+void __weak arch_scale_set_curr_freq(int cpu, unsigned long freq) {}
+
+void __weak arch_scale_set_max_freq(int cpu, unsigned long freq) {}
+
 static void __cpufreq_notify_transition(struct cpufreq_policy *policy,
 		struct cpufreq_freqs *freqs, unsigned int state)
 {
@@ -313,6 +317,7 @@ static void __cpufreq_notify_transition(struct cpufreq_policy *policy,
 		pr_debug("FREQ: %lu - CPU: %lu", (unsigned long)freqs->new,
 			(unsigned long)freqs->cpu);
 		trace_cpu_frequency(freqs->new, freqs->cpu);
+		arch_scale_set_curr_freq(freqs->cpu, freqs->new);
 		srcu_notifier_call_chain(&cpufreq_transition_notifier_list,
 				CPUFREQ_POSTCHANGE, freqs);
 		if (likely(policy) && likely(policy->cpu == freqs->cpu))
@@ -1996,7 +2001,7 @@ EXPORT_SYMBOL(cpufreq_get_policy);
 static int cpufreq_set_policy(struct cpufreq_policy *policy,
 				struct cpufreq_policy *new_policy)
 {
-	int ret = 0, failed = 1;
+	int ret = 0, failed = 1, cpu;
 
 	pr_debug("setting new policy for CPU %u: %u - %u kHz\n", new_policy->cpu,
 		new_policy->min, new_policy->max);
@@ -2036,6 +2041,13 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 
 	policy->min = new_policy->min;
 	policy->max = new_policy->max;
+	
+	for_each_cpu(cpu, policy->cpus) {
+               arch_scale_set_max_freq(cpu, policy->max);
+               /* Make sure that current frequency gets set at init */
+               arch_scale_set_curr_freq(cpu, policy->cur);
+    }
+
 	trace_cpu_frequency_limits(policy->max, policy->min, policy->cpu);
 
 	pr_debug("new min and max freqs are %u - %u kHz\n",
