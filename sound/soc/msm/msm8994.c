f89ccda841de3a6a31d8869e36d592e589dd2e97
Tristan Marsell <tristan.marsell@t-online.de>
2017-07-14 12:55:48 +0200

    sound: PDesireAudio: Time for PDesireAudio 11.0 Auralia
    Changelog:
    -API got more functions and changed from base
    --> Take care: it is not uhqa_mode_pdesireaudio anymore, now it is PDesireAudio
    -Header is placed now on a better position
    -Header is now fully Linux compliant
    -Fixed some failures which CAF caused
    -Buffer increase for LPA, PDesireAudio should not throttle anymore on LPA state
    -Misc patches
    
    Signed-off-by: Tristan Marsell <tristan.marsell@t-online.de>

--- sound/soc/msm/msm8994.c
+++ sound/soc/msm/msm8994.c
@@ -27,7 +27,7 @@
 #include <linux/input.h>
 #include <linux/module.h>
 #include <linux/switch.h>
-#include <linux/pdesireaudio/api.h>
+#include <sound/pdesireaudio/api.h>
 #include <sound/core.h>
 #include <sound/soc.h>
 #include <sound/soc-dapm.h>
