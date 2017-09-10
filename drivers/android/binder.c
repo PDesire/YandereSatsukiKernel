dc66a2a16f1faedd61848ae1cc60d78c90116a10
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-09-11 01:16:09 +0200

    Revert "android: binder: remove binder.h"
    This reverts commit 218b98075611469f56a215cacb5c665c57f440bb.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -38,11 +38,7 @@
 #include <linux/slab.h>
 #include <linux/pid_namespace.h>
 
-#ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
-#define BINDER_IPC_32BIT 1
-#endif
-
-#include <uapi/linux/android/binder.h>
+#include "binder.h"
 #include "binder_trace.h"
 
 static DEFINE_MUTEX(binder_main_lock);
